b83dbd2e4e684003f7bf8f0a0c09903ba95ae4e6
akhilesh2903 <akhileshbhat13@gmail.com>
2026-04-17 11:50:12 +0530

    deploy

--- app/academics/mba/explore/mbaExploreContent.tsx
+++ app/academics/mba/explore/mbaExploreContent.tsx
@@ -898,7 +898,7 @@ export default function MbaExploreContent() {
   }
 
   const facultyYearOrder = Array.from(
-    new Set(mbaFacultyAchievements.map((a) => a.year))
+    new Set(mbaFacultyAchievements.map((a) => a.year)),
   ).sort((a, b) => b.localeCompare(a, undefined, { numeric: true }));
 
   const facultyAchievementsByYear = facultyYearOrder.map((year) => ({
