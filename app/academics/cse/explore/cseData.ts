4231d23f0faa73a3da1ec9b1ac6d041b429d2675
praneethk000 <praneethkpoojary000@gmail.com>
2026-06-11 11:28:26 +0530

    minor change

--- app/academics/cse/explore/cseData.ts
+++ app/academics/cse/explore/cseData.ts
@@ -67,7 +67,7 @@ export const cseDepartmentData: DepartmentData = {
       phone: "NA",
       aoi: "Computer Vision, Image Processing, Computational Neural sciences",
       photo: imageProxyUrl(
-        "https://drive.google.com/uc?export=view&id=1SrT-hbGjTconxf0bTSs2L8tYi9zqHJWF/"
+        "https://drive.google.com/uc?export=view&id=1SrT-hbGjTconxf0bTSs2L8tYi9zqHJWF"
       ),
       joiningDate: "18-11-2020",
       details: {
