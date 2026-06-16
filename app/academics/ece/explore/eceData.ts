67297fafd8d0103f2ced4ccbcdb53af9a3c43250
praneethk000 <praneethkpoojary000@gmail.com>
2026-06-16 16:05:32 +0530

    Update:minor change

--- app/academics/ece/explore/eceData.ts
+++ app/academics/ece/explore/eceData.ts
@@ -16,7 +16,7 @@ M4:	By imbibing the students with human values and ethics through transformative
 
 `,
   hod: {
-    name: "Dr. Keshaveni",
+    name: "Dr. Keshaveni N",
     designation: "Professor & HOD - ECE",
     qualification: "Ph.D",
     experience: "15+ Years",
